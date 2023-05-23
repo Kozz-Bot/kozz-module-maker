8a994a9b54a9039f0dfdb925cc47b898ae2242e5
Guilherme Tramontano <tramontano.guilherme@yahoo.com.br>
2023-05-23 21:50:04 +0000

    chore: removing sample handler

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,2 @@
-import { createHandlerInstance } from './Instance';
-import { createMethod } from './Schema';
-
 export * from './Instance';
 export * from './Schema';
-
-createHandlerInstance({
-	name: 'debug',
-	address: `http://gramont.ddns.net:4521`,
-	methods: {
-		...createMethod({
-			name: 'default',
-			args: {},
-			func: (requester, args) => {
-				requester.reply.withText('wasdasd');
-			},
-		}),
-	},
-});
