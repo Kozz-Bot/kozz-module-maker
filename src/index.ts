8d8c5a8dfecfeab761925dae41d3200819530ced
Guilherme Tramontano <tramontano.guilherme@yahoo.com.br>
2023-08-02 23:02:08 +0000

    fix: solving unresolvable promises for non-existing resources

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,4 @@
-import { createHandlerInstance } from './Instance/Handler';
-
-import dotenv from 'dotenv';
-dotenv.config();
-
 export * from './Instance/Handler';
 export * from './Instance/Proxy';
 export * from './Instance/Basic';
 export * from './Schema';
-
-const testHandler = createHandlerInstance({
-	name: 'debug',
-	address: `${process.env.GATEWAY_URL}`,
-	methods: {},
-	boundariesToHandle: ['postman-test'],
-}).resources.upsertResource('help', () => {
-	return new Promise(resolve => {
-		setTimeout(() => resolve('done'), 1500);
-	});
-});
