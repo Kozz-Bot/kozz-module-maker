699844205c5ba18fa8fd7d1f4511181547be52c9
Guilherme Tramontano <tramontano.guilherme@yahoo.com.br>
2023-09-12 07:03:16 +0000

    feat: remade the function for creating methods

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,4 @@
-import { createHandlerInstance } from './Instance/Handler';
-import { loadTemplates } from './Message';
-import { createMethod } from './Schema';
-
 export * from './Instance/Handler';
 export * from './Instance/Proxy';
 export * from './Instance/Basic';
 export * from './Schema';
-
-const defaultMethod = createMethod(
-	'default',
-	(requester, args) => {
-		console.log(args);
-		requester.reply('pong');
-	},
-	{
-		test: 'number',
-	}
-);
-
-const templatePath = './src/Handlers/Ping/reply.kozz.md';
-
-createHandlerInstance({
-	boundariesToHandle: ['Gramonta-Wa', 'postman-test', 'postman-test-2'],
-	name: 'ping',
-	address: `http://gramont.ddns.net:4521`,
-	methods: {
-		...defaultMethod,
-	},
-	templatePath,
-}).resources.upsertResource('help', () =>
-	loadTemplates(templatePath).getTextFromTemplate('Help')
-);
