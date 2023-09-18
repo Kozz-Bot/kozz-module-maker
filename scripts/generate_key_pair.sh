e0bc4a5e4891dfdc3502ac7eac475fd38b0d0bfe
Guilherme Tramontano <tramontano.guilherme@yahoo.com.br>
2023-09-18 03:37:40 +0000

    feat: updating script to not crash if keys folder doesnt exist

--- scripts/generate_key_pair.sh
+++ scripts/generate_key_pair.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env sh
 
+folder_path="./keys"
+
+if [ ! -d "$folder_path" ]; then
+  mkdir -p "$folder_path"
+fi
+
 openssl genrsa -out ../keys/privatekey.pem 2048
 openssl rsa -in ../keys/privatekey.pem -out ../keys/publickey.pem -pubout -outform PEM
