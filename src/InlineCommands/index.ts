f081a58d61430f8d9f86946895553b0c27757efc
Guilherme Tramontano <tramontano.guilherme@yahoo.com.br>
2024-03-31 03:25:47 +0000

    feat: inline-commands

---
+++ src/InlineCommands/index.ts
@@ -0,0 +1,27 @@
+/**
+ * Creates an inline command with unespecified name and payload data.
+ * @param commandName
+ * @param data
+ */
+export const generalCommand = (
+	commandName: string,
+	data: Record<string, any> = {}
+) => {
+	return `%${commandName}:${JSON.stringify(data)}`;
+};
+
+/**
+ * Tags someone in the message. The tag will be visible for everyone
+ * @param id
+ */
+export const tagMember = (id: string) => generalCommand('mention', { id });
+
+/**
+ * Tags someone in the message. The tag WILL NOT be visible for anyone
+ * @param id
+ * @returns
+ */
+export const invisibleTagMember = (id: string) =>
+	generalCommand('invisiblemention', { id });
+
+export const tagEveryone = () => generalCommand('tageveryone');
